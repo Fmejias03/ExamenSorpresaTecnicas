599048907123fc93d8a16cfd5503fe2be6725bf4
Fernando <fmeji@hotmail.es>
2023-02-09 20:18:55 +0100

    Ejrcicios

--- src/com/company/DemoTest.java
+++ src/com/company/DemoTest.java
@@ -5,12 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class DemoTest {
 
 
-        @org.junit.jupiter.api.Test
-        void main() {
-            Caja caja = new CajaMac();
-            Boton boton = new DialogoMac();
-            boton.mostrar();
-            caja.mostrar();
-        }
+    @org.junit.jupiter.api.Test
+    void main() {
+        Caja caja = new CajaMac();
+        Boton boton = new DialogoMac();
+        boton.mostrar();
+        caja.mostrar();
+    }
 
 }
